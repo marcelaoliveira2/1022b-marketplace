6455dd0412ba55bde718ccaa8f29c1d9c22f15c0
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2024-10-21 08:25:34 -0400

    Update main.ts
    Adicionando código dos usuários na main

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,25 @@ app.get("/produtos",async(req,res)=>{
 }
 })
 
+app.get("/usuarios",async(req,res)=>{
+    try{
+        const conexao = await mysql.createConnection({
+         host: process.env.dbhost?process.env.dbhost:"localhost",
+         user: process.env.dbuser?process.env.dbuser:"root",
+         password: process.env.dbpassword?process.env.dbpassword:"",
+         database: process.env.dbname?process.env.dbname:"banco1022b",
+         port: process.env.dbport?parseInt(process.env.dbport):3306
+        })
+
+        const [result,fields] = await conexao.query("SELECT * FROM usuarios");
+        await conexao.end()
+
+        res.send(result);
+    }catch(e){
+        res.status(500).send("Erro do servidor")
+    }
+})
+
     
 
 
