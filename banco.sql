e90e54c05beade8c33c0ff0347bd0732fdb39b53
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2024-10-21 08:24:57 -0400

    Update banco.sql
    Adicionando código dos usuários no banco de dados

--- banco.sql
+++ banco.sql
@@ -7,4 +7,16 @@ CREATE TABLE IF NOT EXISTS produtos(
     imagem VARCHAR(300)
 );
 INSERT INTO produtos VALUES
-(1,'Iphone','Celular RUIM',5000.50,'https://images.tcdn.com.br/img/img_prod/616573/apple_iphone_14_pro_5g_6gb_ram_tela_6_1_1015_3_89b1382594eaffcd5ba54081bfeb9244.jpg');
+(1,'Iphone','Celular RUIM',5000.50,'https://images.tcdn.com.br/img/img_prod/616573/apple_iphone_14_pro_5g_6gb_ram_tela_6_1_1015_3_89b1382594eaffcd5ba54081bfeb9244.jp');
+
+
+USE defaultdb;
+CREATE TABLE IF NOT EXISTS usuarios(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    email VARCHAR(50),
+    create_at TIMESTAMP,
+    updated TIMESTAMP
+);
+INSERT INTO usuarios VALUES
+(1,'Maria das Graças de Oliveira', 'maria.oliveira7@gmail.com','2024-10-21','2024-10-21');
