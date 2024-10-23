de46e6d840e5668adf8b77b6bd4e7e5589843db8
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2024-10-23 07:10:46 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS produtos(
 INSERT INTO produtos VALUES
 (1,'Iphone','Celular RUIM',5000.50,'https://images.tcdn.com.br/img/img_prod/616573/apple_iphone_14_pro_5g_6gb_ram_tela_6_1_1015_3_89b1382594eaffcd5ba54081bfeb9244.jp');
 (2,'Samsung', 'Celular BOM', 6950.00, 'https://m.media-amazon.com/images/I/71PazrXqw8L._AC_SX679_.jpg');
-(2),'Motorola', 'Celular MAIS OU MENOS', 999.63, 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTkZN7MClV1O5rzez3iS3RPn92cQfx3I8RA21z91ZA-qEjJv52v2aNKXt8O1bxzwbV1rgXv5DYc4x9xcEwEn1gARHY6a39VugGJuSzDBC1YUuX6tOIckDfq-A&usqp=CAE';
+(3,'Motorola', 'Celular MAIS OU MENOS', 999.63, 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTkZN7MClV1O5rzez3iS3RPn92cQfx3I8RA21z91ZA-qEjJv52v2aNKXt8O1bxzwbV1rgXv5DYc4x9xcEwEn1gARHY6a39VugGJuSzDBC1YUuX6tOIckDfq-A&usqp=CAE');
 
 
 USE defaultdb;
