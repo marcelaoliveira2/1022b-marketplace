fc069aebddd7dd0d84c68971101a3e137a0e69b1
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2024-12-18 08:07:05 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,28 +13,30 @@ app.use(express.json())  //Middleware
 //incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
 app.use(cors())
 //ROTAS
+import BancoMysql from './db/bancoMysql'
+
 app.get("/produtos",async(req,res)=>{
 
     //O que eu tenho que fazer aqui dentro?
     //OK -> PASSO 1: Criar o banco de dados
     //PASSO 2: Usar a lib mysql2 para conectar com o banco
     try{
-        const conexao = await mysql.createConnection({
-            host: process.env.dbhost?process.env.dbhost:"localhost",
-            user:process.env.dbuser?process.env.dbuser:"root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
-            database:process.env.dbname?process.env.dbname:"banco1022b",
-            port:process.env.dbport?parseInt(process.env.dbport):3306
-        })
-        //PASSO 3: QUERY  -> SELECT * FROM produtos
-        const [result,fields]  = await conexao.query("SELECT * FROM produtos")
-        await conexao.end()
-        //PASSO 4: Colocar os dados do banco no response
+        //Criamos um objeto do tipo BancoMysql
+        //Na hora que é criado o construtor é executado
+        const banco = new BancoMysql();
+        const result = await banco.query()
+        console.log(result)
+        await banco.end()
         res.send(result)
     }catch(e){
         res.status(500).send("Erro do servidor")
     }  
 })
+
+
+
+
+
 
 app.post("/produtos",async(req,res)=>{
     try{
