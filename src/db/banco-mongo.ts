54a147db120716c9209f2edeba863d654038e412
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2025-01-21 08:17:42 -0400

    Delete banco-mongo.ts

--- src/db/banco-mongo.ts
+++ src/db/banco-mongo.ts
@@ -3,7 +3,6 @@ import { MongoClient } from 'mongodb';
 //const url = 'mongodb://localhost:27017';
 //const client = new MongoClient(url);
 //const dbName = 'banco1022b';
-//await client.connect();
 //const db = client.db(dbName);
 class BancoMongo{
     private client:MongoClient
