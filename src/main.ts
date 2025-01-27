faa9f0ace9a0c1066cd9f73b3b98b07f031a558e
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2025-01-27 08:28:23 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -30,13 +30,26 @@ app.get("/produtos",async(req,res)=>{
     }  
 })
 
+app.get("/produtos/:id",async(req,res)=>{
+    try{
+        const banco = new BancoMongo();
+        const result = await banco.listarPorId(req.params.id)
+        console.log(result)
+        await banco.end()
+        res.send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
 app.post("/produtos",async(req,res)=>{
     try{
         const {id,nome,descricao,preco,imagem} = req.body
         console.log(id,nome,descricao,preco,imagem)
         const banco = new BancoMongo();
 
-        const produto = {id,nome,descricao,preco,imagem}
+        const produto = {id:parseInt(id),nome,descricao,preco,imagem}
 
         const result = await banco.inserir(produto)
         console.log(result)
